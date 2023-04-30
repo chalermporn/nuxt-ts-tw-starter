a3df4f53778cc16150893139879eba420168d726
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-04-30 11:17:56 +0700

    feat: tailwind config

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <div class="text-primary-100">Hello</div>
+    <div class="text-red-400">Hello</div>
+    <div class="text-green-400">Hello</div>
   </div>
 </template>
